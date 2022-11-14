3c05e9e1c65cba894221307e3eaedfa328b09980
Sujan <abuhanif.sujan@hyperisland.se>
2022-11-14 21:09:35 +0100

    displaying card dynamically

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 "use strict"
-const cards = document.querySelectorAll('.card');
-console.log(cards);
-const displayCard = [];
+// const cards = document.querySelectorAll('.img-cards');
+
+
+const displayCards = [
+    './card-pic/king-of-clubs.png',
+    './card-pic/king-of-diamonds.png',
+    './card-pic/king-of-hearts.png',
+    './card-pic/king-of-spades.png',
+    './card-pic/queen-of-clubs.png',
+    './card-pic/queen-of-diamonds.png',
+    './card-pic/queen-of-hearts.png',
+    './card-pic/queen-of-spades.png'];
+
+const showCardsEl = document.getElementById('showCards');
+
+
+
+displayCards.forEach((element, i) => {
+    showCardsEl.innerHTML += ` 
+    
+        <img src="${element}" alt="">
+        `;
+})
+
+function shuffleCards() {
+    let index = Math.floor(Math.random() * displayCard.length);
+    let allImage = displayCards[index];
+    document.querySelectorAll('.img').src = `./card-pic/${allImage}`;
+}
